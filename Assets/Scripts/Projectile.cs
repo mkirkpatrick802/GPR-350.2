0420992b1ab064c811aa9ae4627d8bb1ca345e12
Michael Kirkpatrick <mkirkpatrick802@gmail.com>
2023-09-15 21:30:51 -0400

    done

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,20 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float speed;
+    public bool slingBack;
+    public bool deadBrop;
+    public bool flyer;
+
+    private void Start()
+    {
+        if (slingBack) gameObject.GetComponent<Particle2D>().acceleration = new Vector2(-15, -10);
+        if (deadBrop) StartCoroutine(DropTimer());
+        if (flyer) gameObject.GetComponent<Particle2D>().acceleration = new Vector2(0, 10);
+    }
+
+    IEnumerator DropTimer()
+    {
+        yield return new WaitForSeconds(.25f);
+        gameObject.GetComponent<Particle2D>().acceleration = new Vector2(0, -300);
+    }
 }
